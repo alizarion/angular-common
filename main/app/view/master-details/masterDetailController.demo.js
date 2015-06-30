4c9f8078b60f02d2a15744aa790fc1132b09ecb6
alizarion <selim@openlinux.fr>
2015-06-30 15:12:04 +0200

    Master details vue test

--- main/app/view/master-details/masterDetailController.demo.js
+++ main/app/view/master-details/masterDetailController.demo.js
@@ -1,10 +1,47 @@
 angular.module('itesoft').controller('MasterDetailController',
     ['$scope', 'MasterDetailDataService', function($scope, MasterDetailDataService) {
 
-    $scope.data = MasterDetailDataService.data;
+        $scope.data = MasterDetailDataService.data;
 
-    $scope.displayDetail = function (item) {
-      $scope.currentItem = item;
-    };
+        $scope.currentItemWrapper = null;
 
-}]);
+        $scope.$watch('currentItemWrapper.currentItem', function(newValue,oldValue){
+            console.log(newValue);
+            console.log(oldValue);
+
+            if($scope.currentItemWrapper!=null ){
+                if(typeof newValue !== "undefined" && typeof oldValue !== "undefined") {
+
+
+                        if (!angular.equals(newValue, oldValue)) {
+                            console.log('watch') ;
+
+                            $scope.currentItemWrapper.hasChange = true;
+                        }
+                }
+            }
+        }, true);
+
+        $scope.displayDetail = function (item,index) {
+            console.log('displayDetail') ;
+            if($scope.currentItemWrapper != null){
+                if($scope.currentItemWrapper.hasChange){
+                    return;
+                }
+
+            }
+            $scope.currentItemWrapper = {
+                "currentIndex":index,
+                "currentItem" :item,
+                "hasChange":false
+            };
+
+        };
+        $scope.saveCurrent =  function(){
+            if($scope.currentItemWrapper!=null){
+                $scope.currentItemWrapper.hasChange =false;
+            }
+        };
+
+
+    }]);
