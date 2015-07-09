e0f6e51de045017f66fc7bc7e9d35e4af6bcc0fb
alizarion <selim@openlinux.fr>
2015-07-09 16:13:55 +0200

    update master-detail grid resizing class

--- main/app/composite/master-detail/master.js
+++ main/app/composite/master-detail/master.js
@@ -87,13 +87,130 @@ function ngGridLayoutPlugin () {
  * @example
     <example module="itesoft-showcase">
         <file name="index.html">
-            <it-master-detail>
-                <it-master>
-                    <it-master-header>
-                    </it-master-header>
-                </it-master>
-            </it-master-detail>
+                 <it-master-detail >
+                 <it-master it-master-data="data" it-lang="'fr'" it-master-detail-control="masterDetails" it-lock-on-change="true" it-en>
+                 <it-master-header it-search-placeholder="Recherche" >
+                 <button class="btn btn-primary" title="Add" ng-disabled="currentItemWrapper.hasChanged" ng-click="addNewItem()"><span class="fa fa-plus fa-lg"></span></button>
+                 <button class="btn btn-danger" title="Delete" ng-disabled="currentItemWrapper.hasChanged" ng-click="deleteSelectedItems()"><span class="fa fa-trash fa-lg"></span></button>
+                 <button class="btn btn-success" ng-disabled="currentItemWrapper.hasChanged" title="Down"><span class="fa fa-chevron-down fa-lg"></span></button>
+                 <button class="btn btn-success" ng-disabled="currentItemWrapper.hasChanged" title="Up"><span class="fa fa-chevron-up fa-lg"></span></button>
+                 </it-master-header>
+                 </it-master>
+                 <it-detail>
+                 <it-detail-header>
+                 <button class="btn btn-warning" title="Save"  ng-disabled="!currentItemWrapper.hasChanged" ng-click="saveCurrentItem()">
+                 <span class="fa fa-floppy-o fa-lg"></span>
+                 </button>
+                 <button class=" btn btn-info" title="Check">
+                 <span class="fa fa-file-code-o fa-lg"></span>
+                 </button>
+                 <button class="btn btn-success" title="Undo" ng-click="undoChange()">
+                 <span class="fa fa-undo fa-lg"></span>
+                 </button>
+
+                 </it-detail-header>
+                 <it-detail-content>
+                 <div class="form-group">
+                 <input it-input type="text" class="form-control floating-label" id="priorityDescription"
+                 it-text="code"
+                 ng-model="currentItemWrapper.currentItem.code"
+                 name=""
+                 ng-required="true"/>
+                 </div>
+                 <div class="form-group">
+                 <input it-input type="text" class="form-control floating-label" id="priorityCategory"
+                 it-text="description"
+                 ng-model="currentItemWrapper.currentItem.description" name=""/>
+                 </div>
+                 <div class="form-group">
+                 <input type="checkbox"
+                 it-toggle
+                 ng-model="currentItemWrapper.currentItem.enabledde"
+                 it-text="tete"/>
+                 </div>
+                 </it-detail-content>
+                 </it-detail>
+                 </it-master-detail>
         </file>
+         <file name="controller.js">
+             angular.module('itesoft-showcase')
+
+             .controller('MasterDetailController', ['$scope', function($scope) {
+
+                    $scope.data =
+                       [
+                            {
+                                "code" : "Code 1",
+                                "description": "Description 1",
+                                "enabledde" : true
+                            },
+                            {
+                                "code" : "Code 2",
+                                "description": "Description 2",
+                                "enabledde" : false
+                            },
+                            {
+                                "code" : "Code 3",
+                                "description": "Description 3",
+                                "enabledde" : true
+                            },
+                            {
+                                "code" : "Code 4",
+                                "description": "Description 4",
+                                "enabledde" : false
+                            },
+                            {
+                                "code" : "Code 5",
+                                "description": "Description 5",
+                                "enabledde" : true
+                            }
+                        ];
+
+                    $scope.masterDetails = {};
+
+                    $scope.masterDetails = {
+                        columnDefs : [{ field: 'code', displayName: 'My value 1',  width: '8%', sortable:true},
+                            { field: 'description', displayName: 'My value 2',  width: '10%', sortable:true},
+                            { field: 'enabledde', displayName: 'My value 3',   sortable:false}]
+
+                    };
+
+                    function _removeItems(items,dataList){
+                        angular.forEach(items,function(entry){
+                            var index = dataList.indexOf(entry);
+                            dataList.splice(index, 1);
+                        })
+                    }
+
+                    $scope.deleteSelectedItems = function(){
+                        _removeItems($scope.masterDetails.getSelectedItems(), $scope.data);
+                    };
+
+                    $scope.saveCurrentItem = function(){
+                        $scope.$broadcast('unlockCurrentItem');
+                    };
+                    $scope.undoChange = function(){
+                        $scope.masterDetails.undoChangeCurrentItem();
+                    };
+
+                    $scope.addNewItem = function(){
+                        var newItem =  {
+                            "code" : "Code " + ($scope.data.length+1) ,
+                            "description": "Description " + ($scope.data.length+1),
+                            "enabledde" : true
+                        };
+                        $scope.data.push(newItem);
+                        $scope.masterDetails.setCurrentItem(newItem).then(function(success){
+                            $scope.$broadcast('lockCurrentItem');
+                        },function(error){
+
+                        });
+                    }
+                }]);
+
+           </file>
+            <file src="test.css" >
+            </file>
     </example>
  */
 IteSoft
@@ -114,7 +231,7 @@ IteSoft
                 '</div>'+
                 '<div class="row">'+
                 '<div class="col-md-12">'+
-                ' <div ng-grid="gridOptions"  class="gridStyle">' +
+                ' <div ng-grid="gridOptions"  class="it-master-detail-grid">' +
                 '</div>'+
                 '</div>'+
                 '</div>'+
