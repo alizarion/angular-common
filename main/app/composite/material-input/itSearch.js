863b746a9a8ddc3dd6449600a103d1518f4dd1e7
flofpo <florence.pomme@itesoft.com>
2015-10-06 12:07:46 +0200

    itSearch : composant de recherche sur une liste ; la liste peut-être présentée dans un composant ui-grid ou une table

--- main/app/composite/material-input/itSearch.js
+++ main/app/composite/material-input/itSearch.js
@@ -8,129 +8,162 @@
  * @description
  * An element providing a single filter box that searches across multiple columns in a grid (ui-grid) or a table.
  *
- * You MUST pass an empty object `<it-search it-search-control="searchControl"></it-search>`
- * this object will
+ * You MUST pass an object `<it-search it-search-control="searchControl"></it-search>`.
+ * This object will be used as following:
  * <table class="table">
  *  <tr>
  *   <td><code>searchControl.columnFields = { <br/> columnFields : ['field1', 'field2', 'field3']  <br/>}</code></td>
  *   <td>Object passed to the multicolumns function filter inside the component to let it know on which columns to apply the filter.</td>
  *  </tr>
  *  <tr>
- *   <td><code>searchControl.multicolumnsFilter()</code></td>
- *   <td>Method to filter in the grid or table according the choosen column fields.</td>
+ *   <td><code>searchControl.multicolumnsFilter(renderableRows)</code></td>
+ *   <td>Method to filter in the grid or table according the choosen column fields.<br/>It returns the new rows to be displayed.</td>
  *  </tr>
  * </table>
- * You MUST pass a function `<it-search it-filter="filter()"></it-search>`
- * this function will be executed at each new input ; it should call searchControl.multicolumnsFilter() to refresh the displayed data and has to be written in the application controller.
+ * You MUST also pass a function `<it-search it-filter="filter()"></it-search>`.
+ * This function will be executed at each new input ; it should call searchControl.multicolumnsFilter() to refresh the displayed data and has to be written in the application controller.
  *
  * @usage
  * <it-search it-placeholder="Recherche multicolonnes" it-search-control="searchControl" it-filter="filter()">
  * </it-search>
  *
  * @example
- <example module="itesoft-showcase">
- <file name="index.html">
- <div ng-controller="SearchDemoController">
- <div class="container-fluid">
-     <div class="jumbotron">
-         <div class="row" ng-show="mode==0">
-            <it-search it-placeholder="Recherche multicolonnes dans UI-GRID" it-search-control="searchControl" it-filter="filter(0)"></it-search>
-            <span class="btn btn-default"><b>UI-GRID list</b></span><button class="btn btn-primary" ng-click="switch(1)">Switch to TABLE</button>
-            <div ui-grid="photoGrid" id="photoGrid"></div>
+     <example module="itesoft-showcase">
+         <file name="Module.js">
+         angular.module('itesoft-showcase',['ngResource','itesoft']);
+         </file>
+         <file name="LatinService.js">
+         angular.module('itesoft-showcase')
+         .factory('Latin',['$resource', function($resource){
+                                                    return $resource('http://jsonplaceholder.typicode.com/posts');
+                                                }]);
+         </file>
+         <file name="Controller1.js">
+         angular.module('itesoft-showcase')
+         .controller('SearchDemoControllerGrid',['$scope','Latin', function($scope,Latin) {
+                            $scope.searchControl = {};
+                            $scope.searchControl = {
+                                columnFields : ['title', 'body']
+                            };
+
+                            $scope.dataSource = [];
+
+                            //---------------ONLY UI-GRID--------------------
+                            $scope.myDefs = [
+                                    {
+                                        field: 'id',
+                                        width: 50
+                                    },
+                                    {
+                                        field: 'title'
+                                    },
+                                    {
+                                        field: 'body'
+                                    }
+                            ];
+                            $scope.latinGrid = {
+                                data: 'dataSource',
+                                columnDefs: $scope.myDefs,
+                                onRegisterApi: function (gridApi) {
+                                    $scope.gridApi = gridApi;
+                                    $scope.gridApi.grid.registerRowsProcessor($scope.searchControl.multicolumnsFilter, 200);
+                                }
+                            };
+                            //---------------ONLY UI-GRID--------------------
+
+                            $scope.filter = function () {
+                                $scope.gridApi.grid.refresh();
+                            };
+
+                            $scope.loadDataGrid = function() {
+                                $scope.dataSource = [];
+
+                                Latin.query().$promise
+                                .then(function(data){
+                                    $scope.dataSource = data;
+                                });
+                            };
+                     }]);
+         </file>
+         <file name="index.html">
+         <div ng-controller="SearchDemoControllerGrid">
+         <div class="container-fluid">
+             <div class="jumbotron">
+                 <div class="row">
+                    <button class="btn btn-primary" ng-click="loadDataGrid()">DISPLAY DATA IN UI-GRID</button>
+                    <it-search it-placeholder="Recherche multicolonnes dans UI-GRID" it-search-control="searchControl" it-filter="filter()"></it-search>
+                    <div ui-grid="latinGrid" id="latinGrid"></div>
+                 </div>
+             </div>
          </div>
-         <div class="row" ng-show="mode==1">
-            <it-search it-placeholder="Recherche multicolonnes dans TABLE" it-search-control="searchControl" it-filter="filter(1)"></it-search>
-            <span class="btn btn-default"><b>TABLE list</b></span><button class="btn btn-primary" ng-click="switch(0)">Switch to UI-GRID</button>
-             <table class="table table-striped table-hover ">
-             <thead>
-                <tr><th>id</th><th>title</th><th>url</th><th>image</th></tr>
-             </thead>
-             <tbody>
+         </div>
+         </file>
+
+     </example>
+
+     <example module="itesoft-showcase1">
+         <file name="index.html">
+             <div ng-controller="SearchDemoControllerTable">
+                 <div class="container-fluid">
+                 <div class="jumbotron">
+                 <div class="row">
+                 <button class="btn btn-primary" ng-click="loadDataTable()">DISPLAY DATA IN TABLE</button>
+                 <it-search it-placeholder="Recherche multicolonnes dans TABLE" it-search-control="searchControl" it-filter="filter()"></it-search>
+                 <table class="table table-striped table-hover ">
+                 <thead>
+                 <tr><th>id</th><th>title</th><th>body</th></tr>
+                 </thead>
+                 <tbody>
                  <tr ng-repeat="dataItem in data">
-                     <td>{{dataItem.id}}</td>
-                     <td>{{dataItem.title}}</td>
-                     <td>{{dataItem.url}}</td>
-                     <td><img ng-src="{{dataItem.thumbnailUrl}}" alt="">{{dataItem.body}}</td>
+                 <td>{{dataItem.id}}</td>
+                 <td>{{dataItem.title}}</td>
+                 <td>{{dataItem.body}}</td>
                  </tr>
-             </tbody>
-             </table>
-         </div>
-     </div>
- </div>
- </div>
- </file>
- <file name="Module.js">
- angular.module('itesoft-showcase',['ngResource','itesoft']);
- </file>
- <file name="PhotosService.js">
- angular.module('itesoft-showcase')
- .factory('Photos',['$resource', function($resource){
-                                return $resource('http://jsonplaceholder.typicode.com/photos/:id',null,{});
-                            }]);
- </file>
- <file name="Controller.js">
- angular.module('itesoft-showcase')
- .controller('SearchDemoController',['$scope','Photos','$timeout', function($scope,Photos,$timeout) {
-        $scope.searchControl = {};
-        $scope.searchControl = {
-            columnFields : ['title', 'url']
-        };
+                 </tbody>
+                 </table>
+                 </div>
+                 </div>
+                 </div>
+             </div>
+         </file>
+         <file name="Module1.js">
+            angular.module('itesoft-showcase1',['ngResource','itesoft']);
+         </file>
+         <file name="LatinService1.js">
+             angular.module('itesoft-showcase1')
+             .factory('Latin1',['$resource', function($resource){
+                                            return $resource('http://jsonplaceholder.typicode.com/posts');
+                                        }]);
+         </file>
+         <file name="Controller2.js">
+             angular.module('itesoft-showcase1')
+             .controller('SearchDemoControllerTable',['$scope','Latin1', function($scope,Latin1) {
+                    $scope.searchControl = {};
+                    $scope.searchControl = {
+                        columnFields : ['title', 'body']
+                    };
 
-        //---------------ONLY UI-GRID--------------------
-        $scope.myDefs = [
-                {
-                    field: 'id',
-                    width: 50
-                },
-                {
-                    field: 'title'
-                },
-                {
-                    field: 'url'
-                }
-        ];
-        $scope.photoGrid = {
-            data: 'dataSource',
-            columnDefs: $scope.myDefs,
-            onRegisterApi: function (gridApi) {
-                $scope.gridApi = gridApi;
-                $scope.gridApi.grid.registerRowsProcessor($scope.searchControl.multicolumnsFilter, 200);
-            }
-        };
-        //---------------ONLY UI-GRID--------------------
+                    $scope.dataSource = [];
+                    $scope.data = [];
 
-        $scope.filter = function (mod) {
-            if (mod === 0) {//UI-GRID
-                $scope.gridApi.grid.refresh();
-            }
-            else {//TABLE
-                $scope.data = $scope.searchControl.multicolumnsFilter($scope.dataSource);
-            }
+                    $scope.filter = function () {
+                        $scope.data = $scope.searchControl.multicolumnsFilter($scope.dataSource);
+                    };
 
-        };
+                    $scope.loadDataTable = function() {
+                        $scope.dataSource = [];
+                        $scope.data = [];
 
-        $scope.switch = function (mod) {
-            $scope.mode = mod;
-            $scope.searchControl.filterText = "";//Empty it-search input
-            $scope.loadAutoData();
-        };
+                        Latin1.query().$promise
+                        .then(function(data){
+                           $scope.dataSource = data;
+                           $scope.data = data;
+                        });
+                    };
+             }]);
+         </file>
 
-        $scope.loadAutoData = function() {
-            $scope.dataSource = [];
-            $scope.data = [];//TABLE
-
-            Photos.query().$promise
-            .then(function(data){
-                $scope.dataSource = data;
-                $scope.data = data;//TABLE
-            });
-        }
-        $scope.switch(0);//initialized for UI-GRID
- }]);
- </file>
-
- </example>
- *
+     </example>
  **/
 IteSoft
     .directive('itSearch',function() {
