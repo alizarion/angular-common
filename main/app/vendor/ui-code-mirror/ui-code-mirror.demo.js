2f357998155d58a017471e3daf8824b99e748f80
stephen zambaux <stephen.zambaux@gmail.com>
2016-04-11 11:17:28 +0200

    Correction de la doc de l'UI code mirror

--- main/app/vendor/ui-code-mirror/ui-code-mirror.demo.js
+++ main/app/vendor/ui-code-mirror/ui-code-mirror.demo.js
@@ -11,22 +11,24 @@
  * Provides an Code Editor Component
  * See more :  {@link https://github.com/angular-ui/ui-codemirror}
  * @example
-     <example module="itesoft">
+     <example module="itesoft-showcase">
          <file name="Module.js">
             angular.module('itesoft-showcase',['ui.codemirror','itesoft']);
          </file>
         <file name="index.html">
+            <div ng-controller="mainController">
              <textarea ui-codemirror="editorOptions" name="json data"
              class="it-fill form-control floating-label"
              type="text"
              placeholder="Enter content"
              ng-model="model">
              </textarea>
+            </div>
         </file>
          <file name="Controller.js">
          angular.module('itesoft-showcase')
              .controller('mainController', ['screenSize','$scope', function (screenSize,$scope) {
-               $scope.model= "Write your script here";
+               $scope.model= '{"toto":{"toto":{"titi":3}}}';
                $scope.editorOptions = {
                         lineWrapping: true,
                         lineNumbers: true,
@@ -36,6 +38,7 @@
                         gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"],
                         styleActiveLine: true,
                         mode: "application/json",
+                        theme: "material",
                         lint: true
                     };
           }]);
