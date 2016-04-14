e0cc84e852133d634caa4604da080764741e96da
alizarion <selim@openlinux.fr>
2016-04-14 17:11:28 +0200

    fix modal example issue

--- main/app/composite/full-screen/itModalFullScreen.js
+++ main/app/composite/full-screen/itModalFullScreen.js
@@ -24,13 +24,14 @@
  * @example
  <example module="itesoft">
      <file name="index.html">
-        <div konami style="height:500px">
+
          <it-modal-full-screen  class="it-fill">
              <div class="jumbotron it-fill" >Lorem ipsum dolor sit amet,
              consectetur adipisicing elit.  Assumenda autem cupiditate dolor dolores dolorum et fugiat inventore
              ipsum maxime, pariatur praesentium quas sit temporibus velit, vitae. Ab blanditiis expedita tenetur.
              </div>
          </it-modal-full-screen>
+        <div konami style="height:500px">
         </div>
      </file>
 
