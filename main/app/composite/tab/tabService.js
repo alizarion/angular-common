08426f0a0f736f61ad3b8c2f90c0d84f91d61f6f
lds <lds@itesoft.com>
2016-10-05 15:22:11 +0200

    add notion of 'group' in ittab and ittab contant

--- main/app/composite/tab/tabService.js
+++ main/app/composite/tab/tabService.js
@@ -47,8 +47,8 @@
  </file>
  <file name="controller.js">
  angular.module('itesoft-showcase').controller('HomeCtrl', ['$scope', function($scope) {
-    TabService.onTabChanged(function (selectedTabId) {
-              self.isActiveTab = (selectedTabId == self.id);
+    TabService.onTabChanged(function (selectedTabId,groupId) {
+              self.isActiveTab = TabService.isTabActive(selectedTabId, groupId);
           });
  }]
  );
@@ -57,34 +57,76 @@
  */
 
 itTab.factory('TabService', [function () {
-        var self = this;
-        self.tabChangedCallBacks = [];
-        self.changeTab = _changeTab;
-        self.onTabChanged = _onTabChanged;
-        self.currentActiveTabId = "";
+    var self = this;
 
-        /**
-         * Change current active tab
-         * @param newTabId
-         * @private
-         */
-        function _changeTab(newTabId) {
-            self.currentActiveTabId = newTabId;
-            self.tabChangedCallBacks.forEach(function (callBack) {
-                if (angular.isDefined(callBack)) {
-                    callBack(newTabId);
-                }
-            })
+    var _DEFAULT_TAB_GROUP = '_IT_DEFAULT_TAB_GROUP';
+
+    self.tabChangedCallBacks = [];
+    self.changeTab = _changeTab;
+    self.onTabChanged = _onTabChanged;
+    self.isTabActive = _isTabActive;
+    self.currentActiveTabIds = [];
+
+    /**
+     * Change current active tab
+     * @param newTabId id of the tab
+     * @param groupId id of the group
+     * @private
+     */
+    function _changeTab(newTabId, groupId) {
+        console.log('_changeTab (' + newTabId + ',' + groupId + ')' + '] ...');
+        if (checkGroupId(groupId)) {
+            console.log('before self.currentActiveTabIds[' + _DEFAULT_TAB_GROUP + '] = ' + self.currentActiveTabIds[_DEFAULT_TAB_GROUP]);
+            self.currentActiveTabIds[_DEFAULT_TAB_GROUP] = newTabId;
+            console.log('after self.currentActiveTabIds[' + _DEFAULT_TAB_GROUP + '] = ' + self.currentActiveTabIds[_DEFAULT_TAB_GROUP]);
+        } else {
+            self.currentActiveTabIds[groupId] = newTabId;
         }
 
-        /**
-         * Register listener changed
-         * @param callBack
-         * @private
-         */
-        function _onTabChanged(callBack) {
-            self.tabChangedCallBacks.push(callBack)
-        }
+        console.log('self.currentActiveTabIds[' + groupId + '] = ' + self.currentActiveTabIds[groupId]);
+        console.log('self.tabChangedCallBacks = ' + self.tabChangedCallBacks);
+        self.tabChangedCallBacks.forEach(function (callBack) {
+            if (angular.isDefined(callBack)) {
+                console.log('Executing callback [' + callBack + '(' + newTabId + ',' + groupId + ')' + '] ...');
+                callBack(newTabId, groupId);
+                console.log('Executing callback [' + callBack + '(' + newTabId + ',' + groupId + ')' + '] DONE.');
+            }
+        });
+        console.log('_changeTab (' + newTabId + ',' + groupId + ')' + '] DONE.');
 
-        return self;
-    }]);
+    }
+
+    function checkGroupId(groupId) {
+        return !angular.isDefined(groupId) || groupId == null || groupId === '';
+    }
+
+
+    /**
+     * Does the tab is active
+     * @param tabId the identifier of the tab
+     * @param groupId the identifier of the tab group
+     * @returns {boolean}
+     * @private
+     */
+    function _isTabActive(tabId, groupId) {
+        var result = false;
+
+        if (checkGroupId(groupId)) {
+            result = self.currentActiveTabIds[_DEFAULT_TAB_GROUP] === tabId;
+        } else {
+            result = self.currentActiveTabIds[groupId] === tabId;
+        }
+        return result;
+    }
+
+    /**
+     * Register listener changed
+     * @param callBack
+     * @private
+     */
+    function _onTabChanged(callBack) {
+        self.tabChangedCallBacks.push(callBack)
+    }
+
+    return self;
+}]);
